f6a464960a5d4dbb89a9a0bd9f83a75fb28b4f47
Zach Barnes <zachary.barnes.32@gmail.com>
2024-06-11 16:37:25 -0600

    Initial styling and basic functionality

---
+++ app.js
@@ -0,0 +1,95 @@
+let menu = [
+    {
+        name: "vanilla",
+        price: 2.50,
+        quantity: 0
+    }, 
+    {
+        name: "chocolate",
+        price: 3.50,
+        quantity: 0
+    }, 
+    {
+        name: "strawberry",
+        price: 4.50,
+        quantity: 0
+    },
+    {
+        name: "wild berry",
+        price: 5.00,
+        quantity: 0
+    },
+    {
+        name: "sprinkles",
+        price: 0.50,
+        quantity: 0
+    },
+    {
+        name: "berries",
+        price: 2.50,
+        quantity: 0
+    }, 
+    {
+        name: "chocolate chips",
+        price: 1.00,
+        quantity: 0
+    },
+    {
+        name: "gummy bears",
+        price: 1.50,
+        quantity: 0
+    },
+    {
+        name: "waffle cone",
+        price: 1.50,
+        quantity: 0
+    },
+    {
+        name: "waffle bowl",
+        price: 2.50,
+        quantity: 0
+    },
+    {
+        name: "pint",
+        price: 4.00,
+        quantity: 0
+    }, 
+    {
+        name: "bowl",
+        price: 3.00,
+        quantity: 0
+    }
+]
+
+function buyVanilla() {
+    menu[0].quantity += 1;
+    console.log("bought a scoop of vanilla", menu[0]);
+}
+
+function buyChocolate() {
+    menu[1].quantity += 1;
+    console.log("bought a scoop of chocolate", menu[1]);
+}
+
+function addToOrderByName(itemName) {
+    for (let i = 0; i < menu.length; i++) {
+        if (itemName == menu[i].name) {
+            menu[i].quantity += 1;
+           // console.log("added item", menu[i])
+        }
+    }
+    drawOrder();
+}
+
+function drawOrder(){
+    const checkoutElem = document.getElementById('checkout');
+    let checkoutContent = ''
+    for (let i = 0; i < menu.length; i++){
+        if (menu[i].quantity > 0) {
+            checkoutContent += `<p> ${menu[i].quantity} ${menu[i].name} $${(menu[i].price * menu[i].quantity).toFixed(2)}</p>`
+        }
+        // console.log(checkoutContent);
+        checkoutElem.innerHTML = checkoutContent;
+
+    }
+}
